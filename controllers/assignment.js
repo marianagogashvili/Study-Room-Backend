25beafc847ec4ef3a1d4539e46905d9bacaa22e7
Mariana Gogashvili <marianagogashvili24@gmail.com>
2021-02-03 22:42:51 +0200

    edit assignment

--- controllers/assignment.js
+++ controllers/assignment.js
@@ -8,8 +8,8 @@ exports.createAssignment = async (req, res, next) => {
 		const description = req.body.description;
 		const courseId = req.body.courseId;
 		const topicId = req.body.topicId;    
-		const availableFrom = req.body.availableFrom;
-		const deadline = req.body.deadline;
+		let availableFrom = new Date(req.body.availableFrom).setHours(new Date(req.body.availableFrom).getHours() + 2);
+		let deadline = new Date(req.body.deadline).setHours(new Date(req.body.deadline).getHours() + 2) || null;
 
 		let fileUrls = [];
 		req.files.forEach(file => {
@@ -58,6 +58,48 @@ exports.getById = async (req, res, next) => {
 
 		res.status(200).json(assignment);
 	} catch (err) {
+		if (!err.statusCode) {
+	      err.statusCode = 500;
+	    }
+		next(err);
+	}
+}
+
+exports.editAssignment = async (req, res, next) => {
+	try {
+		const id = req.body.id;
+		const title = req.body.title;
+		const description = req.body.description;
+		const availableFrom = new Date(req.body.availableFrom).setHours(new Date(req.body.availableFrom).getHours() + 2);
+		const deadline = new Date(req.body.deadline).setHours(new Date(req.body.deadline).getHours() + 2) || null;
+
+		const remove = JSON.parse(req.body.remove);
+
+		const assignment = await Assignment.findById(id).populate('topic');
+
+		let newFiles = [...assignment.fileUrl];
+		remove.forEach(r =>  {
+			console.log(r);
+			newFiles = newFiles.filter(file => file !== r);
+		});
+		
+		
+		req.files.forEach(file =>  newFiles.push(file.path));
+
+		assignment.title = title;
+		assignment.description = description;
+		assignment.availableFrom = availableFrom;
+		assignment.deadline = deadline;
+		assignment.fileUrl = newFiles;
+
+		await assignment.save();
+
+		remove.forEach(file => {
+			fs.unlinkSync(file);
+		});
+		
+		res.status(201).json(assignment);
+	}  catch (err) {
 		if (!err.statusCode) {
 	      err.statusCode = 500;
 	    }
